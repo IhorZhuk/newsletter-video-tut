8961b6c36c2988dbcccd4f458a72b05261a0723f
IhorZhuk <ihor.zhuk92@gmail.com>
2016-08-26 18:56:51 +0200

    updated gulpfile added gulp-inline-source

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,29 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     inky = require('inky'),
-    inlineCss = require('gulp-inline-css');
+    inlineCss = require('gulp-inline-css'),
+    inlinesource = require('gulp-inline-source');
 
 
 //STYLES
 gulp.task('styles', function () {
-  return gulp.src('./scss/styles.scss')
+  return gulp.src('./scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./css'));
 });
 
 //CONVERTE INKY
-gulp.task('inky', function() {
+gulp.task('inky', ['styles'], function() {
   return gulp.src('./templates/**/*.html')
+    .pipe(inlinesource())
     .pipe(inky())
+    .pipe(inlineCss({
+        preserveMediaQueries: true
+    }))
     .pipe(gulp.dest('./dist'));
 });
 
-//INLINE CSS
-gulp.task('inline', function () {
-  return gulp.src('./dist/*.html')
-        .pipe(inlineCss())
-        .pipe(gulp.dest('./dist/inlined'));
-});
-
-
 //WATCH
 gulp.task('default',function() {
-    gulp.watch('./scss/**/*.scss',['styles']);
-    gulp.watch('./templates/**/*.html',['inky']);
+    gulp.watch(['./scss/*.scss', './templates/**/*.html'],['inky']);
 });
